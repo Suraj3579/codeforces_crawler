bbaaef653dc71f9750ca48a444b1aca527c68b5b
chinmaianandh <chinmaianandh906@gmail.com>
2020-07-18 15:14:33 +0530

    Calendar for CodeForces

--- CF_Crawler/CodeCalender/views.py
+++ CF_Crawler/CodeCalender/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
+import urllib3
+import json
 
 
 # Create your views here.
 
 
 def code_calender(request):
-    # contest_list =
-    # context = {'contest_list': contest_list}
-    context = {}
+    http = urllib3.PoolManager()
+    r = http.request('GET', 'https://codeforces.com/api/contest.list?gym=false')
+    # print(r.status)
+    contest_list = json.loads(r.data.decode('utf-8'))
+    contest_list = contest_list["result"]
+    contest_list = contest_list[:100]
+    # for cont in contest_list:
+    #     print(cont)
+    context = {'contest_list': contest_list}
     return render(request, 'calender.html', context)
