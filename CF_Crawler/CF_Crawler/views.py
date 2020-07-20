82b6ce4c5191e4dc0f4eba2730cddc5506cad2b6
chinmaianandh <chinmaianandh906@gmail.com>
2020-07-20 11:04:51 +0530

    user not found implemented for search

--- CF_Crawler/CF_Crawler/views.py
+++ CF_Crawler/CF_Crawler/views.py
@@ -20,12 +20,19 @@ def user_handle(request):
         http = urllib3.PoolManager()
         u = http.request('GET', ('https://codeforces.com/api/user.info?handles='+form))
         userinfo_list = json.loads(u.data.decode('utf-8'))
-        userinfo_list = userinfo_list["result"]
-        userinfo_list = userinfo_list[0]
-        print(userinfo_list)
-        context1 = {'userinfo_list': userinfo_list}
+        status=userinfo_list['status']
+        if status == 'OK':
+            print("user found")
+            status = True
+            userinfo_list = userinfo_list["result"]
+            userinfo_list = userinfo_list[0]
+        else:
+            print("user not found")
+            status = False
+            userinfo_list = userinfo_list['comment']
+            print("yoyo "+userinfo_list)
+        context1 = {'userinfo_list': userinfo_list, 'status': status}
         # return HttpResponseRedirect('/userhandle')
     else:
-        print("not found")
         form = UserHandle()
     return render(request, 'userinfo.html', context1)
