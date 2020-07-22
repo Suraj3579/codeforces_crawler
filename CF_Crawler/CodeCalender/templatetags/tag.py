8f0a4115f27661bfa180d6f11cb3bc513391326c
chinmaianandh <chinmaianandh906@gmail.com>
2020-07-22 15:27:46 +0530

    Time converted to IST

--- CF_Crawler/CodeCalender/templatetags/tag.py
+++ CF_Crawler/CodeCalender/templatetags/tag.py
@@ -1,22 +1,24 @@
 from django import template
 import datetime
+import pytz
+ist = pytz.timezone("Asia/Kolkata")
 
 register = template.Library()
 
 
 @register.simple_tag
 def convert_time(time_stamp):
-    timestamp = datetime.datetime.fromtimestamp(time_stamp)
+    timestamp = datetime.datetime.fromtimestamp(time_stamp, ist)
     return timestamp.strftime('%d-%m-%Y %H:%M')
 
 
 @register.simple_tag
-def get_count(tagcount,tag):
+def get_count(tagcount, tag):
     out = [tagcount[tag]]
     return out
 
 
 @register.simple_tag
-def get_ABC_count(tagcount,tag):
+def get_ABC_count(tagcount, tag):
     out = [tagcount[tag]]
     return out[0]
