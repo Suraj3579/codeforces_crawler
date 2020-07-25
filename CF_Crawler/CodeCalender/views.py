349040a663300f49040025ab3cad3bc10ab3c7f2
jayaprakashjp12 <jayaprakashreddy3263@gmail.com>
2020-07-25 13:28:34 +0530

    codechef scrapping done

--- CF_Crawler/CodeCalender/views.py
+++ CF_Crawler/CodeCalender/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 import urllib3
 import json
-
-
-# Create your views here.
+####
+from bs4 import BeautifulSoup
+import requests
 
 
 def code_calender(request):
@@ -23,7 +23,39 @@ def code_calender(request):
     contest_list.sort(key=fun)
     # for cont in contest_list:
     #     print(cont)
-    context = {'contest_list': contest_list, 'string': string, 'link_contest': link_contest}
+
+    soup = requests.get('https://www.codechef.com/contests/?itm_medium=navmenu&itm_campaign=allcontests#future-contests')
+    soup=BeautifulSoup(soup.content,'html')
+    soup=soup.find(id='future-contests')
+    soup=soup.next_sibling.next_sibling
+    soup=soup.find_all('td')
+    result=[]
+    links=[]
+    for tag in soup:
+    #print (tag.text)
+        result.extend(tag.stripped_strings)
+        link=tag.find('a')
+        if link!=None :
+            link=link.get('href')
+            links.append(link)
+    chef_contest_list=[]
+    count=len(links)
+    print (count)
+    for i in range (count):
+        list=[]
+        list.append(links[0])
+        links.pop(0)
+        result.pop(0)
+        for j in range(3):
+            list.append(result[0])
+            result.pop(0)
+        result.pop(0),result.pop(0)
+        chef_contest_list.append(list)
+    
+    print(chef_contest_list)
+    chef_link="https://www.codechef.com"
+    context = {'contest_list': contest_list, 'string': string, 'link_contest': link_contest,'chef_contest_list':chef_contest_list,
+                'chef_link':chef_link }
     return render(request, 'calender.html', context)
 
  
