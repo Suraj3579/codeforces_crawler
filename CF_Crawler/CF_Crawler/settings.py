acb803b440a8cae541a6487f95dedfaf3940a399
Chinmai Anandh Chappa <52035224+chinmaianandh@users.noreply.github.com>
2020-08-01 10:36:39 +0530

    email password changed

--- CF_Crawler/CF_Crawler/settings.py
+++ CF_Crawler/CF_Crawler/settings.py
@@ -129,6 +129,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'CodeCrawler906@gmail.com'
-EMAIL_HOST_PASSWORD = 'scvbbutfhvupddrq'
+EMAIL_HOST_PASSWORD = 'nsuxtsfzdvaqzaqv'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
